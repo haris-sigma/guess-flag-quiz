30906babfd25269bf4fa3bd97038a2399c4e258e
sigmaschooltech <126048372+haris-sigma@users.noreply.github.com>
2023-03-21 09:33:14 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function startQuiz() {
+
+  const guess = prompt('Where is this flag 🇺🇸 from?');
+  
+  if (guess.toLowerCase() === 'usa') {
+    alert('You guessed correctly!');
+  } else {
+    alert('Sorry, the correct answer was USA.');
+  }
+  
+  const guess2 = prompt('Where is this flag 🇸🇬 from?');
+  
+  if (guess2.toLowerCase() === 'singapore') {
+    alert('You guessed correctly!');
+  } else {
+    alert('Sorry, the correct answer was Singapore.');
+  }
+}
+
+
